d346cf0606d4cc6ed2ee90f50119721b5c7b4008
Marshall <32920499+YungMoose@users.noreply.github.com>
2019-07-21 00:44:31 -0600

    Initial commit of ACP redo

--- src/app/ACP.java
+++ src/app/ACP.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package app;
 
 import java.util.HashMap;
 import java.util.Scanner;
 
-/**
- *
- * @author Cole
- */
+
 public class ACP {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         AppControl.mapCommand("name", new DisplayName());
         AppControl.mapCommand("number", new DisplayNumber());
--- src/app/DisplayText.java
+++ src/app/DisplayText.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package app;
 
 import java.util.HashMap;
 
-/**
- *
- * @author Cole
- */
+
 public class DisplayText implements Handler{
     @Override
     public void handleIt(HashMap<String, Object> dataMap){
